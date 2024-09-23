debc8124c137194bcd493996d9a3843e32e1e08f
Niño Rey Noble <139203404+xinoux@users.noreply.github.com>
2024-09-23 19:25:09 +0800

    Made responsive

--- src/HomePage/components/sideText.js
+++ src/HomePage/components/sideText.js
@@ -1,15 +1,6 @@
-export default function SideText({label,positionSide = 'left', origin = 'left top'}){
+export default function SideText({label,positionSide = 'left'}){
     const sideTextStyle = {
-        position: 'absolute',
-        [positionSide]: '2rem',  
-        bottom: '19rem',         
-        transform: 'rotate(-90deg)',
-        transformOrigin: origin,  
-        fontFamily: 'Playfair Display, serif',
-        fontWeight: 600,
-        textTransform: 'uppercase',
-        color: '#414141',
-        letterSpacing: '0.2rem',
+        [positionSide]: '-2rem',           
     };
             
     return(
--- src/HomePage/skills.js
+++ src/HomePage/skills.js
@@ -7,23 +7,25 @@ export default function Skills({ skillsRef }) {
 
   return (
     <section className="skills" id="skills" ref={skillsRef}>
-      <div className="serviceCardRow">
-        <ServiceCard image="html" label="HTML" />
-        <ServiceCard image="css" label="CSS" />
-        <ServiceCard image="javascript" label="Javascript" />
-        <ServiceCard image='java' label='Java'/>
+      <div className="serviceCardRowContainer">
+        <div className="serviceCardRow">
+          <ServiceCard image="html" label="HTML" />
+          <ServiceCard image="css" label="CSS" />
+          <ServiceCard image="javascript" label="Javascript" />
+          <ServiceCard image='java' label='Java'/>
+        </div>
+        <div>
+          <SideText label='Skills' positionSide={"left"}/>
+        </div>
+        <div className="serviceCardRow">
+        
+          <ServiceCard image="react" label="React JS" />
+          <ServiceCard image="c++" label="C++" />
+          <ServiceCard image="csharp" label="C#" />
+          <ServiceCard image='git' label='Git'/>
+        </div>
       </div>
-      <div className="serviceCardRow">
       
-        <ServiceCard image="react" label="React JS" />
-        <ServiceCard image="c++" label="C++" />
-        <ServiceCard image="csharp" label="C#" />
-        <ServiceCard image='git' label='Git'/>
-
-      </div>
-      <div>
-        <SideText label='Skills' positionSide={"right"} origin={"right bottom"}/>
-      </div>
     </section>
   );
 }
